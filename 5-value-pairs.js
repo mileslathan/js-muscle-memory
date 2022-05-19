11e56df18fd61df4071424f39603693e5337245e
mileslathan <mlathan.cn@gmail.com>
2022-05-19 11:52:13 -0400

    Question 5 Answered.

--- 5-value-pairs.js
+++ 5-value-pairs.js
@@ -11,7 +11,14 @@ const object2 = {name: 'Two', location: 'San Francisco'};
 valuePair(object1, object2, 'location'); // => [ 'Remote', 'San Francisco' ]
 valuePair(object1, object2, 'name'); // => [ 'One', 'Two' ]
 ***********************************************************************/
+const object1 = {name: 'One', location: 'Remote', age: 1};
+const object2 = {name: 'Two', location: 'San Francisco'};
 
 function valuePair(obj1, obj2, key) {
-
+    let newArray = [];
+    newArray.push(`${obj1[key]}`);
+    newArray.push(`${obj2[key]}`);
+    return newArray
 }
+
+console.log(valuePair(object1, object2, "location"));
