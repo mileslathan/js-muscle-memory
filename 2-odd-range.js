3d3625fd9fb0329ce7ccec76c6237d094cae313e
mileslathan <mlathan.cn@gmail.com>
2022-05-18 21:55:22 -0400

    Question 2 Answered.

--- 2-odd-range.js
+++ 2-odd-range.js
@@ -9,5 +9,13 @@ oddRange(6); // => [ 1, 3, 5 ]
 ******************************************************************************/
 
 function oddRange(end) {
-
-}
+    let odds = 0;
+   
+       for (let i = 0; i < end.length; i++) {
+       if (end[i] % 2 !== 0) {
+           console.log(end[i]);
+       }
+       }
+   }
+   
+   oddRange([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
