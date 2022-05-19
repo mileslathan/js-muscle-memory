79503eeb7e3e78e44f7535c167e1a91295826e04
mileslathan <mlathan.cn@gmail.com>
2022-05-19 11:51:16 -0400

    Question 4 answered

--- 4-cat-builder.js
+++ 4-cat-builder.js
@@ -4,13 +4,20 @@ object with the corresponding properties.
 
 Example:
 
-const cat1 = catBuilder('Garfield', 'golden', ['scratching-post', 'yarn']);
+
 cat1; // => { name: 'Garfield', color: 'golden', toys: ['scratching-post', 'yarn'] }
 
 const cat2 = catBuilder('Whiskers', 'rainbow', ['poptarts']);
 cat2; // => { name: 'Whiskers', color: 'rainbow', toys: [ 'poptarts' ] }
 ***********************************************************************/
 
-function catBuilder(name, color, toys) {
-
+class catBuilder {
+    constructor(name, color, toys) {
+        this.name = name
+        this.color = color
+        this.toys = toys
+    }
 }
+
+newCat = new catBuilder("garfield", "golden", ["scratching-post", "yarn"]);
+console.log(newCat);
